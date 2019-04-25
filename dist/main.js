19647e8a0fd5a7a732ab9401046e185842a944e3
Luiz Daniel Lima <luizdml@gmail.com>
2019-04-25 17:18:21 -0300

    first well rounded version

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
-let mySlider = new Glide('.glide', {
-  type: 'carousel',
-  gap: 40,
-  perView: 2
-});
+// let mySlider = new Glide('.glide', {
+//   type: 'carousel',
+//   gap: 40,
+//   perView: 3
+// });
 
-mySlider.mount();
+// mySlider.mount();
